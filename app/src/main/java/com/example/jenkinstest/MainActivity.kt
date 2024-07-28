653921a28105ccf47ec8a0a610b6c22a21b2b5be
AbhijeetInstAbhi <singhabhijeet105@gmail.com>
2024-07-28 15:21:55 +0530

    Second Commit

--- app/src/main/java/com/example/jenkinstest/MainActivity.kt
+++ app/src/main/java/com/example/jenkinstest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jenkinstest
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.txtvw)
+        textView.setOnClickListener {
+            textView.text = "JenkinsTest"
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
